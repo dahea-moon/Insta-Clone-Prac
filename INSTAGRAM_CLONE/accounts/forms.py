981dd326fa8b0688b3c3746fe982ed63a5c4acf8
Dahea Moon <dahea0512@gmail.com>
2020-02-25 17:17:05 +0900

    signup, login/out, userpage, following done

--- INSTAGRAM_CLONE/accounts/forms.py
+++ INSTAGRAM_CLONE/accounts/forms.py
@@ -5,13 +5,13 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class CustomeUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
         fields = ('username', 'email')
 
 
-class CustomeAuthenticationForm(AuthenticationForm):
+class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         model = User
